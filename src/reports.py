f16a62f271ed3e8ecc78b01400d0f0b778382099
Ex_person <baranovs11@yandex.ru>
2024-11-13 02:13:47 +0300

    Переписаны и упрощены функции и тесты

--- src/reports.py
+++ src/reports.py
@@ -1,4 +1,5 @@
 import logging
+from idlelib.iomenu import encoding
 
 from typing import Optional
 
@@ -8,32 +9,41 @@ from datetime import datetime, timedelta
 
 from statistics import mean
 
-from utils import get_data_from_xlsx
-from decorator import log
+
+logger = logging.getLogger("currency")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/reports.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s_%(funcName)s:%(lineno)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
     """
     Функция формирующая отчет по операциям, фильтруя по категориям, за 3 месяца
     """
-    if date is None:
-        date = datetime.now()
-    else:
-        date = datetime.strptime(date, "%d.%m.%Y")
+    transactions_dict = transactions.to_dict("records")
+    logger.info(f"Преобразование даты {date}")
+    try:
+        if date is None:
+            date = datetime.now()
+        else:
+            date = datetime.strptime(date, "%d.%m.%Y")
+    except Exception:
+        raise ValueError("Не правильный формат даты")
     date_to = date
     date_from = (date_to - timedelta(days=90))
+    logger.info("Получение даты 3 месяца назад")
 
     try:
-        transactions_dict = get_data_from_xlsx(transactions)
-    except Exception as fn:
-        raise FileNotFoundError("Файл не найден")
-
-    try:
+        logger.info("Получение списка транзакций")
         filtered_transactions = [transaction for transaction in transactions_dict if date_to >= datetime.strptime(
                 transaction["Дата операции"], "%d.%m.%Y %H:%M:%S") >= date_from
                                  and transaction["Категория"] == category]
-        return filtered_transactions
-    except Exception:
+        df = pd.DataFrame(filtered_transactions)
+        return df
+    except Exception as ex:
+        logger.info(f"Ошибка при получении списка транзакций: {ex}")
         raise ValueError("Не верный формат данных")
 
 
@@ -42,17 +52,17 @@ def spending_by_weekday(transactions: pd.DataFrame, date: Optional[str] = None) 
     Функция формирующая отчет по средней сумме операций по дням недели
     за 3 месяца от заданной даты
     """
-    if date is None:
-        date = datetime.now()
-    else:
-        date = datetime.strptime(date, "%d.%m.%Y")
+    transactions_dict = transactions.to_dict("records")
+    try:
+        if date is None:
+            date = datetime.now()
+        else:
+            date = datetime.strptime(date, "%d.%m.%Y")
+    except Exception:
+        raise ValueError("Не правильный формат даты")
     date_to = date
     date_from = (date_to - timedelta(days=90))
 
-    try:
-        transactions_dict = get_data_from_xlsx(transactions)
-    except Exception as fn:
-        raise FileNotFoundError("Файл не найден")
     try:
         day_list = [
             {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [], "Saturday": [], "Sunday": []}
@@ -66,7 +76,8 @@ def spending_by_weekday(transactions: pd.DataFrame, date: Optional[str] = None) 
                 day_list[0][weekday].append(transaction["Сумма операции"])
 
         avg_daily_expenses = [{k: round(-mean(v),2) if len(v) != 0 else 0 for k, v in day_list[0].items()}]
-        return avg_daily_expenses
+        df = pd.DataFrame(avg_daily_expenses)
+        return df
     except Exception:
         raise ValueError("Не верный формат данных")
 
@@ -79,7 +90,7 @@ def spending_by_workday(transactions: pd.DataFrame, date: Optional[str] = None) 
     weekdays_expenses = [{"Weekday": [], "Days_off": []}]
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     days_off = ["Saturday", "Sunday"]
-    average_daily_expenses = spending_by_weekday(transactions, date)
+    average_daily_expenses = spending_by_weekday(transactions, date).to_dict("records")
     for day, expenses in average_daily_expenses[0].items():
         if day in weekdays:
             weekdays_expenses[0]["Weekday"].append(expenses)
@@ -87,4 +98,5 @@ def spending_by_workday(transactions: pd.DataFrame, date: Optional[str] = None) 
             weekdays_expenses[0]["Days_off"].append(expenses)
 
     avg_weekdays_expenses = [{k: round(mean(v),2) if len(v) != 0 else 0 for k, v in weekdays_expenses[0].items()}]
-    return avg_weekdays_expenses
+    df = pd.DataFrame(avg_weekdays_expenses)
+    return df
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,6 @@ import pandas as pd
 def get_data_from_xlsx(file_path):
     """ Функция для чтения данных xlsx файла """
     df = pd.read_excel(file_path)
-    file_read = df.to_dict("records")
-    return file_read
+    return df
 
+print(get_data_from_xlsx("data/operations_t.xlsx"))
